cf5e21a2228a49918d0618af7cac4672974986d4
Lev <Lev@DESKTOP-4LIIN5Q>
2023-12-15 15:09:17 +0300

    FliuentValidation added

--- ThreeMorons/Model/User.cs
+++ ThreeMorons/Model/User.cs
@@ -1,20 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace ThreeMorons.Model;
 
 public partial class User
 {
     public Guid Id { get; set; }
-
     public string Name { get; set; } = null!;
 
     public string Surname { get; set; } = null!;
 
     public string Patronymic { get; set; } = null!;
-
     public string Login { get; set; } = null!;
-
     public string Password { get; set; } = null!;
 
     public int UserClassId { get; set; }
---
+++ ThreeMorons/Validators/RegistrationValidator.cs
@@ -0,0 +1,16 @@
+﻿using FluentValidation;
+using ThreeMorons.Model;
+
+namespace ThreeMorons.Validators
+{
+    public class RegistrationValidator : AbstractValidator<User>
+    {
+        public RegistrationValidator() 
+        {
+            RuleFor(u => u.Login).MinimumLength(5).MaximumLength(20).Matches("[A-Za-z0-9]+").WithMessage("Логин должен быть 5-20 символов длиной и состоять только из цифр и латинских букв"); ;
+            RuleFor(u => u.Password).MinimumLength(8).MaximumLength(16)
+                                    .Matches("/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\\W)(?!.* ).{8,16}$/")
+                                    .WithMessage("Пароль должен содержать хотя бы одно число от 0 до 9, хотя бы одну маленькую буквук, одну большую букву, один спец. символ, не содержать пробелов, и быть 8-16 символов в длину");
+        }
+    }
+}
